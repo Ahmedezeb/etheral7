8d395720dde137b9987d1c39fe6e5aa0794f78c3
LivelySource <weston.schmidt4@gmail.com>
2018-09-21 17:01:41 -0700

    commands update

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(`~ BotOutHere Commands~ \n\n[Use ${config.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
+    message.channel.send(`Bot Out Here Commands \nFor any further help, direct message _Lively#0286\n[Use ${config.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} | ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
