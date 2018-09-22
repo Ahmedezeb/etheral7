cdf4a8662b72401566fce66f5f8e5bfc7e179105
LivelySource <weston.schmidt4@gmail.com>
2018-09-21 21:27:12 -0700

    come on

--- commands/urban.js
+++ commands/urban.js
@@ -1,19 +1,9 @@
 exports.run = async (client, message) => {
-  const nsfw = client.channels.find('name', 'nsfw')
-
+	
   if (message.channel.nsfw == false){
     return message.reply('due to We Out Here\'s rules, urban can only be used in NSFW channels, as the content of the command may not be appreciate for all ages.')
   }
   
-  if (message.content.startsWith(config.prefix + "define")) {
-    var term = message.content.replace(config.prefix + "define ", "");
-  }
-  if (message.content.startsWith(config.prefix + "urban")) {
-    var term = message.content.replace(config.prefix + "urban ", "");
-  }
-  if (message.content.startsWith(config.prefix + "def")) {
-    var term = message.content.replace(config.prefix + "def ", "");
-  }
   var request = require("request");
   request("http://api.scorpstuff.com/urbandictionary.php?term=" + term, function(error, response, body) {
     if (!error && response.statusCode == 200) {
