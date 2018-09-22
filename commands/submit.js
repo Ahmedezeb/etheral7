af3129818730501312be5bb74fc3f667fe04308b
LivelySource <weston.schmidt4@gmail.com>
2018-09-21 18:49:45 -0700

    new command

---
+++ commands/submit.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let application = args.join(" ").slice(22);
+
+    let applicationEmbed = new Discord.RichEmbed()
+    .setDescription("Reports")
+    .setColor("#15f153")
+    .addField("Applicant", `${message.author} with ID: ${message.author.id}`)
+    .addField("Time", message.createdAt)
+    .addField("Application", application);
+
+    let applicationschannel = message.guild.channels.find(`name`, "applications");
+    if(!applicationschannel) return message.channel.send("Couldn't find applications channel.");
+
+
+    message.delete().catch(O_o=>{});
+    applicationschannel.send(applicationEmbed);
+    applicationschannel.send('<@&472179405687488541>')
+};
+
+ exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+module.exports.help = {
+  name: "submit",
+  description: 'Submit your application',
+  usage: 'submit [application]'
+}
