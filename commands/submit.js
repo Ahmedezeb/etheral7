02d0c99affa16a631e5660f38325dee367041275
LivelySource <weston.schmidt4@gmail.com>
2018-09-21 20:11:12 -0700

    Revert "damnit"
    This reverts commit 29a2fd91293d6b93055d29b4876bed9e1c1603b5.

--- commands/submit.js
+++ commands/submit.js
@@ -9,14 +9,15 @@ module.exports.run = async (bot, message, args) => {
     .setColor("#ff69b4")
     .addField("Applicant", `${message.author} with ID: ${message.author.id}`)
     .addField("Application", application)
-	.setFooter(message.createdAt);
+	.addField("Time", message.createdAt);
 
     let applicationschannel = message.guild.channels.find(`name`, "applications");
     if(!applicationschannel) return message.channel.send("Couldn't find applications channel.");
 
 
     message.delete().catch(O_o=>{});
-    applicationschannel.send(applicationEmbed)
+    applicationschannel.send(applicationEmbed);
+    applicationschannel.send('If you think the application should be accepted react with :white_check_mark:\n If you think the application should be rejected react with :negative_squared_cross_mark:')
 };
 
  exports.conf = {
