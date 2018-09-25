f9eee8efb7a663868017ef92949ad619a4415c13
LivelySource <weston.schmidt4@gmail.com>
2018-09-24 22:40:38 -0700

    damnit

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
 module.exports = member => {
     let guild = member.guild;
-    guild.channels.get('472175445849276416').send(`Wassup **${member.user.username}**:v:!\n Welcome to _*We Out Here*_ :alien:!`);
+    guild.channels.get('472175445849276416').send(`Wassup **${member.user.username}**:v:!\n Welcome to :alien:  _*We Out Here*_ :alien:`);
 };
