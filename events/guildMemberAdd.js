69b5bceb0af4816b89fb89448ead792a3d4207e8
LivelySource <weston.schmidt4@gmail.com>
2018-09-24 21:42:39 -0700

    try it

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
 module.exports = member => {
   const guild = member.guild;
-  guild.defaultChannel.send(`Wassup ${member.user.username}:v:! Welcome to :alien:We Out Here:alien:!`);
+  member.guild.send(`Wassup ${member.user.username}:v:! Welcome to :alien:We Out Here:alien:!`);
 };
