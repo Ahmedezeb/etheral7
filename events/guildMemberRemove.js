52dae6aeca1a0735d5b4ece61b1ab5ed220c2da7
LivelySource <weston.schmidt4@gmail.com>
2018-09-24 22:26:24 -0700

    push

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,4 @@
 module.exports = member => {
     let guild = member.guild;
-    guild.channels.get('472191923856343071').send(`${member.user.username} was beamed up`);
+    guild.channels.get('472191923856343071').send(`${member.user.username} was beamed up ${member.user.tag}`);
 };
