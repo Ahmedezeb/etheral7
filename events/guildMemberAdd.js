7162b1f480d36dee7e11e72e2e4a7edb1f420405
LivelySource <weston.schmidt4@gmail.com>
2018-09-24 19:55:26 -0700

    test

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,16 @@
-module.exports = member => {
-  const guild = member.guild;
-  member.guild.send(`Wassup ${member.user.username}:v:! Welcome to :alien:We Out Here:alien:!`);
+module.exports = (client, member) => {
+
+ // Load the guild's settings
+ const settings = client.getGuildSettings(member.guild);
+
+ // If welcome is off, don't proceed (don't welcome the user)
+ if (settings.welcomeEnabled !== "true") return;
+
+ // Replace the placeholders in the welcome message with actual data
+ const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user.tag);
+
+ // Send the welcome message to the welcome channel
+ // There's a place for more configs here.
+ member.guild.channels.find("name", settings.welcomeChannel).send(welcomeMessage).catch(console.error);
+
 };
