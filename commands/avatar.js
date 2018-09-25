3e5cad77a643a98b83179eecbf1df447fcebb82f
LivelySource <weston.schmidt4@gmail.com>
2018-09-24 22:59:07 -0700

    yeet

---
+++ commands/avatar.js
@@ -0,0 +1,16 @@
+xports.run = (client, message) => {
+    message.reply(message.author.avatarURL);
+    };
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["avy"],
+    permLevel: 0
+};
+
+exports.help = {
+    name: "avatar",   
+    description: "Sends your avatar's URL.",
+    usage: "avatar"
+};
