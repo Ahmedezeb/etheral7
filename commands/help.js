8fd795c27bfedc73bed3b865ed6ddfa942905825
LivelySource <weston.schmidt4@gmail.com>
2018-09-25 21:00:27 -0700

    help updated

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,33 @@
-const config = require('../config.json');
-exports.run = (client, message, params) => {
-  if (!params[0]) {
-    const commandNames = Array.from(client.commands.keys());
+exports.run = (client, message, args, level) => {
+  // If no specific command is called, show all filtered commands.
+  if (!args[0]) {
+    // Filter all commands by which are available for the user's level, using the <Collection>.filter() method.
+    const myCommands = message.guild ? client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level) : client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level &&  cmd.conf.guildOnly !== true);
+
+    // Here we have to get the command names only, and we use that array to get the longest name.
+    // This make the help commands "aligned" in the output.
+    const commandNames = myCommands.keyArray();
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(`<Bot Out Here Commands>\n\nFor any further help, direct message _Lively#0286\n\n[Use ${config.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} | ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
+
+    let currentCategory = "";
+    let output = `= Command List =\n\n[Use ${message.settings.prefix}help <commandname> for details]\n`;
+    const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
+    sorted.forEach( c => {
+      const cat = c.help.category.toProperCase();
+      if (currentCategory !== cat) {
+        output += `\u200b\n== ${cat} ==\n`;
+        currentCategory = cat;
+      }
+      output += `${message.settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
+    });
+    message.channel.send(output, {code: "asciidoc", split: { char: "\u200b" }});
   } else {
-    let command = params[0];
+    // Show individual command's help.
+    let command = args[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.channel.send(`< ${command.help.name} > \n${command.help.description}\nusage::${command.help.usage}`, {code:'asciidoc'});
+      if (level < client.levelCache[command.conf.permLevel]) return;
+      message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\naliases:: ${command.conf.aliases.join(", ")}\n= ${command.help.name} =`, {code:"asciidoc"});
     }
   }
 };
@@ -16,12 +35,13 @@ exports.run = (client, message, params) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['h', 'halp'],
+  aliases: ["h", "halp"],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'help',
-  description: 'Displays all the available commands for your permission level.',
-  usage: 'help [command]'
+  name: "help",
+  category: "System",
+  description: "Displays all the available commands for your permission level.",
+  usage: "help [command]"
 };
