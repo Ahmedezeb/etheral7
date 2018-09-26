dc1f72be457b72acaa89a46e5d4b30ce2ccb5268
LivelySource <weston.schmidt4@gmail.com>
2018-09-25 21:17:07 -0700

    update

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -7,7 +7,7 @@ module.exports.run = (client, message) => {
   .setImage(guild.iconURL)
   .addField('Info for', `**Member count**\n${guild.memberCount}\n**Region**\n${guild.region}\n**Owner**\n${guild.owner}\n**Explicit Filter Level**\n${guild.explicitContentFilter}\n**Guild ID**\n${guild.id}`, inline=true)
   .addField(`${guild}`, `**Default Channel**\n${guild.defaultChannel}\n**AFK Channel**\n${guild.afkChannel}\n**AFK Timeout**\n${afkTimeout}\n**Verification Level**\n${guild.verificationLevel}\n**Created At**\n${guild.createdAt}`, inline=true)
-  .setFooter('invite me with ,invite')
+  .setFooter('Invite your friends > https://discord.gg/reFzBd2')
   message.channel.send({embed})
 };
 exports.conf = {
