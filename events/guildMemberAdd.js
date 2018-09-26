0d36aadaa9bf866a399a351d3986db94e9d91210
LivelySource <weston.schmidt4@gmail.com>
2018-09-25 21:48:58 -0700

    test

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,29 @@
-module.exports = member => {
-    let guild = member.guild;
-    guild.channels.get('472175445849276416').send(`Wassup **${member.user.username}**:v:\n Welcome to :alien:**We Out Here**:alien:`);
-};
+const chalk = require('chalk');
+
+const chalkInfo = chalk.bgBlue.white;
+const chalkWarn = chalk.bgRed.white;
+
+module.exports = (member, client, config) => {
+    const channel = member.guild.channels.find('name', 'welcome');
+    if (!channel){
+        channel = guild.channels.get('472175445849276416');
+    }
+    else{
+        channel.send({embed: {
+            color: 0xffff00,
+            author: {
+            name: member.user.username,
+            icon_url: member.user.avatarURL
+            },
+            thumbnail: {
+            url: member.user.avatarURL
+            },
+            description: `Wassup ${member.user}:v:`.toString(),
+            timestamp: new Date(),
+            footer: {
+            icon_url: client.user.avatarURL,
+            text: "Enjoy your stay at :alien:**We Out Here**:alien:"
+            }
+        }});
+    }
+}
