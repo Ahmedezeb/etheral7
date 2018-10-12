a612c1be1b5c176e1977782bbbcc55402d19e212
LivelySource <weston.schmidt4@gmail.com>
2018-10-12 15:34:18 -0700

    test

--- commands/dm.js
+++ commands/dm.js
@@ -8,6 +8,19 @@ let botmessage = args.join(" ").slice(22)
 rMember.send(botmessage);
 console.log(`Messaged ${rMember.user.username} : ${botmessage}`);
 
+if (message.content.startsWith(config.prefix)) return message.channel.send(":x: Please use commands in real server! :x:") //if the message is a command
+  message.channel.send("This message has been sent :incoming_envelope:");
+  if (message.content.startsWith(config.prefix)) return
+  if (botmessage.length > 256) return message.reply("Your message content too many characters :/") //if the message contnt more than 256 character, what fields do not allow
+  var embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle("Send")
+      .setDescription(`Message to ${rMember.user.username}`)
+      .addField(botmessage, "Send by: " + message.author.username + " with the ID: " + message.author.id)
+      .setTimestamp()
+  bot.guilds.get("472174636839469088").channels.get("500430737275879465").send(embed) //send the embed in a specific channel
+}
+
 message.delete().catch(O_o=>{});
   }
 
