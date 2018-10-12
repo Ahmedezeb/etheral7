6cfea26ea8c074850496c0848fc88f3bbfbf85c0
LivelySource <weston.schmidt4@gmail.com>
2018-10-12 15:40:31 -0700

    send

--- commands/DMALL.js
+++ commands/DMALL.js
@@ -1,4 +1,4 @@
-module.exports.run = async (client, message, args) => {
+module.exports.run = async (bot, message, args) => {
       let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
       if(!message.member.hasPermission("ADMINISTRATOR"))
           return message.reply({embed: {
