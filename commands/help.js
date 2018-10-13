c628e20a97a3149bcb269ba60f21c1a188d10baa
LivelySource <weston.schmidt4@gmail.com>
2018-10-12 21:16:18 -0700

    Old is better

--- commands/help.js
+++ commands/help.js
@@ -4,12 +4,12 @@ exports.run = (client, message, params) => {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
     message.delete().catch(O_o=>{});
-    message.author.send(`For any further help, direct message _Lively#0286\n\n[Use ${config.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} | ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
+    message.channel.send(`For any further help, direct message _Lively#0286\n\n[Use ${config.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} | ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.author.send(`< ${command.help.name} > \n${command.help.description}\nusage::${command.help.usage}`, {code:'asciidoc'});
+      message.channel.send(`< ${command.help.name} > \n${command.help.description}\nusage::${command.help.usage}`, {code:'asciidoc'});
     }
   }
 };
