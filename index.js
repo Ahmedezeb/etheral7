71e5b941b51d6665f1e3e5ff2483df15147fb3b5
LivelySource <weston.schmidt4@gmail.com>
2018-10-12 17:17:07 -0700

    try >>

--- index.js
+++ index.js
@@ -18,6 +18,34 @@ client.on('ready', () => {
     client.user.setActivity('Type .help', {type: 'WATCHING'});
 });
 
+client.on('messageDelete', async (message) => {
+  const logs = message.guild.channels.find('name', 'logs');
+  if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
+      await message.guild.createChannel('logs', 'text');
+  }
+  if (!logs) {
+      return console.log('The logs channel does not exist and cannot be created')
+  }
+  const entry = await message.guild.fetchAuditLogs({
+      type: 'MESSAGE_DELETE'
+  }).then(audit => audit.entries.first())
+  let user;
+  if (entry.extra.channel.id === message.channel.id && (entry.target.id === message.author.id) && (entry.createdTimestamp > (Date.now() - 5000)) && (entry.extra.count >= 1)) {
+      user = entry.executor.username
+  } else {
+      user = message.author
+  }
+  const logembed = new Discord.RichEmbed()
+      //.setTitle('Message Deleted')
+      .setAuthor(user.tag, message.author.displayAvatarURL)
+      .addField(`**Message sent by ${message.author.username}> deleted in ${message.channel.name}**\n\n`, message.content)
+      .setColor(message.guild.member(client.user).displayHexColor)
+      .setFooter(`<#${message.channel.id}>`)
+      .setTimestamp()
+  //console.log(entry)
+  logs.send(logembed);
+});
+
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 fs.readdir('./commands/', (err, files) => {
@@ -64,6 +92,7 @@ client.elevation = message => {
   if (message.author.id === config.ownerid) permlvl = 4;
   return permlvl;
 };
+
 
 
 var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
