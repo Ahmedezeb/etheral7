981d0bfb9913f2660f264336a81a4c01c4fae357
LivelySource <weston.schmidt4@gmail.com>
2018-10-17 17:54:48 -0700

    add

---
+++ commands/meme.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const superagent = require('superagent');
+
+const run = module.exports.run = async (client, msg, args) => {
+  let{body} = await superagent
+  .get(`https://api-to.get-a.life/meme`);
+
+  let me = new Discord.RichEmbed() 
+  .setColor("#7289DA")
+  .setFooter(${message.author})
+  .setImage(body.url);
+
+  msg.channel.send(me);
+    }
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["MEME"],
+    permLevel: 0
+};
+
+exports.help = {
+    name: "meme",   
+    description: "Hungry for memes?",
+    usage: "meme"
+};
