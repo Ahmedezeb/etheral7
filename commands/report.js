801e89bad2b5420009efd8eaba6a67e2f78ce822
Ahmedezeb <47535173+Ahmedezeb@users.noreply.github.com>
2019-02-13 00:16:11 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -20,7 +20,7 @@ module.exports.run = async (bot, message, args) => {
 
     message.delete().catch(O_o=>{});
     reportschannel.send(reportEmbed);
-    reportschannel.send('<@&545013420123226132> <@&545013420123226132>')
+    reportschannel.send('<@#545013420123226132> <@#545013420123226132>')
 };
 
  exports.conf = {
