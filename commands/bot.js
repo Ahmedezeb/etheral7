ce601160d7c91187f028618ad7a82aee618738cf
Ahmedezeb <47535173+Ahmedezeb@users.noreply.github.com>
2019-02-15 17:33:03 +0100

    Update bot.js

--- commands/bot.js
+++ commands/bot.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
-    .setDescription("Simple discord bot, developed by _Lively.")
+    .setDescription("Simple discord bot, developed by DutchArmy")
     .setColor("#15f153")
     .setThumbnail(bicon)
     .addField("Bot Name", bot.user.username)
